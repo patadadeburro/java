40bc9edb340b48b63766a7a07457ec59e875a994
patadadeburro <izthak@yahoo.com.mx>
2017-11-24 18:43:03 -0600

    abecedario
    abecedario

---
+++ act6.java
@@ -0,0 +1,16 @@
+/* 
+Actividad 5. leer 3 numeros e imprimirlos en forma descendente.
+*/
+
+import java.util.Scanner;
+
+import javax.net.ssl.ExtendedSSLSession;
+public class act6{
+	public static void main(String[] args){
+       
+        for( char ch = 'A' ; ch <= 'Z' ; ch++ )
+            System.out.println( ch );
+         
+
+	}
+}	
